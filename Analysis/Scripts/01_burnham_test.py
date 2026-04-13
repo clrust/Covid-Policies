bda6d0d916f280cd3a86af20ff2bab3147b21e65
Connor Rust <clrust@uchicago.edu>
2026-04-13 12:29:56 -0400

    pushing initial burnham testing scripts. want to track all this on git

---
+++ Analysis/Scripts/01_burnham_test.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+import pandas as pd
+import os
+
+
+os.chdir("/Users/connorrust/Library/CloudStorage/Box-Box/Covid Policies/Data")
+data = pd.read_csv("All.csv")
+
+text = data["Text"].str.slice(0,100)
+lst = text.to_list()
+
+hypothesis_template = "This text is about {}"
+classes_verbalized = ["covid", "other"]
+
+zeroshot_classifier = pipeline("zero-shot-classification", model="mlburnham/Political_DEBATE_large_v1.0", device = "mps")  # change the model identifier here
+
+output = zeroshot_classifier(lst, classes_verbalized, hypothesis_template=hypothesis_template, multi_label=False)
+
+clean_output = []
+
+for dct in output:
+    nd = {}
+    nd["sequence"] = dct["sequence"]
+    for idx, label in enumerate(dct["labels"]):
+        nd[label] = dct["scores"][idx]
+    clean_output.append(nd)
+
+df = pd.DataFrame(clean_output)
+
+df.to_csv('MAtopics_ZS_Burnham_modified_prompt_100_500chr.csv')
