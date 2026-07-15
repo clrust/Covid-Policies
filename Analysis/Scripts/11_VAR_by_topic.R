d0af89d6a92ae5fc6ebf72470b6a487b461bb97b
Connor Rust <clrust@uchicago.edu>
2026-07-15 13:54:17 -0400

    porting Brehm modification to use modelsummary to create clean tables with the results of many models

--- Analysis/Scripts/11_VAR_by_topic.R
+++ Analysis/Scripts/11_VAR_by_topic.R
@@ -77,8 +77,33 @@ summary(models$positive_cases) #**
 # ss negative coefficient for Health which doesn't make any sense here
 
 
+### jb 18 jun 2026
+### use modelsummary::modelsummary() to print concise set of results
 
+library(modelsummary)
 
+### I don't know if there is a way to hack the list of topics except manually.
+### This would be worth exploring
+
+renamer <- function(old_names) {
+  new_names <- gsub("vaccines_|housing_|other_|economic_relief_|healthcare_infrastructure_|healthcare_professionals_|reopening_|jobs_|food_|research_|testing_|positive_cases_", "", old_names)
+  new_names <- gsub("_lag1", "[-1]", new_names)
+  setNames(new_names, old_names)
+}
+
+modelsummary(models[1:6],
+             coef_omit="Intercept|^state.*$",
+             coef_rename=renamer,
+             gof_omit="AIC|BIC|R2|R2 Adj.|Log.Lik.|F",
+             output="lag1 by topic pt1.txt")
+
+modelsummary(models[7:12],
+             coef_omit="Intercept|^state.*$",
+             coef_rename=renamer,
+             gof_omit="AIC|BIC|R2|R2 Adj.|Log.Lik.|F",
+             output="lag1 by topic pt2.txt")
+
+#CO, GA, IL, MA, MI, MN, NY, PA, TX, VA
 
 
 
