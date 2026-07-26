3f5d22e2967cda851e9fc7c4842979e78519860f
Connor Rust <clrust@uchicago.edu>
2026-07-25 23:39:28 -0400

    work on implementing both entailment and disentailment scores

--- Analysis/CosSim/09_scatterplots.R
+++ Analysis/CosSim/09_scatterplots.R
@@ -10,20 +10,45 @@ setwd("~/Library/CloudStorage/Box-Box/Covid Policies/Analysis")
 cossim_data <- read_csv("Testing/Results/07_cossim_data.csv")
 
 
-ggplot(cossim_data) +
+gh <- ggplot(cossim_data) +
   geom_point(aes(x = Glag1_H, y = G_Hlag1), alpha = 0.3) +
   geom_abline(slope = 1, intercept = 0,
               linetype = "dashed", color = "red") 
 
-ggplot(cossim_data) +
+gu <- ggplot(cossim_data) +
   geom_point(aes(x = Glag1_U, y = G_Ulag1), alpha = 0.3) +
   geom_abline(slope = 1, intercept = 0,
               linetype = "dashed", color = "red") 
 
-ggplot(cossim_data) +
+hu <- ggplot(cossim_data) +
   geom_point(aes(x = Hlag1_U, y = H_Ulag1), alpha = 0.3) +
   geom_abline(slope = 1, intercept = 0,
               linetype = "dashed", color = "red") 
+
+ggsave(filename = "Testing/Results/ScatterPlot/gh_scatter.png",
+       plot = gh,
+       width = 10,
+       height = 6,
+       units = "in",
+       dpi = 300)
+
+ggsave(filename = "Testing/Results/ScatterPlot/hu_scatter.png",
+       plot = hu,
+       width = 10,
+       height = 6,
+       units = "in",
+       dpi = 300)
+
+
+ggsave(filename = "Testing/Results/ScatterPlot/gu_scatter.png",
+       plot = gu,
+       width = 10,
+       height = 6,
+       units = "in",
+       dpi = 300)
+
+
+
 
 #---- Some plots
 gu <- ggplot(all_states_complete) +
---
+++ Analysis/PosNeg/06_burnham_all_states.py
@@ -0,0 +1,91 @@
+# Created by: CR
+# Date: 4/27/26
+# Script to run Burnham's Political Debate Model v. 1.1 on data from all states
+# Where rescraped data is available, this incorporates the rescraped data without filtering
+
+from transformers import pipeline
+import pandas as pd
+import os
+import numpy as np
+
+### Global Constants; can be changed ###
+LABELS = ["economic relief", "reopening", "jobs", "housing", "vaccines",
+          "testing", "positive cases", "healthcare professionals", 
+          "healthcare infrastructure", "other", "research", "food"]
+MULTI_LABEL = True
+CHARACTER_NUMBER = 100 #number of characers to slice from each press release
+WORKING_DIRECTORY = "/Users/connorrust/Library/CloudStorage/Box-Box/Covid Policies/"
+INPUT_DATA = "Data/05_combine_all_states.csv"
+OUTPUT_PATH = "Analysis/Testing/Results/06_burnham_all_states.csv"
+#####################################
+os.chdir(WORKING_DIRECTORY)
+data = pd.read_csv(INPUT_DATA)
+
+# defining normalization function
+def normalize(matrix, axis=-1):
+    """ Takes a numpy 2D array of topic probabilities and normalizes them. 
+    (This applies L1 not L2 normalization)
+    Args: 
+        matrix(numpy array)
+        axis(int): axis to normalize across, 1: rows; 0: columns
+
+    Returns:
+        2D array with rows/columns normalized
+
+    """
+    return matrix / np.sum(matrix, axis=axis, keepdims = True)
+
+# extracting text from data
+text = data.pop("Text").str.slice(0,CHARACTER_NUMBER)
+lst = text.to_list()
+
+ent_template = "This text is about {}"
+dis_template = "This text is not about {}"
+
+classes_verbalized = LABELS
+
+zeroshot_classifier = pipeline("zero-shot-classification", 
+                               model="mlburnham/Political_DEBATE_DeBERTa_large_v1.1", 
+                               device = "mps")  # change the model identifier here
+
+entailment = zeroshot_classifier(lst, classes_verbalized, 
+                             hypothesis_template=ent_template, 
+                             multi_label=MULTI_LABEL)
+
+disentailment = zeroshot_classifier(lst, classes_verbalized, 
+                             hypothesis_template=dis_template, 
+                             multi_label=MULTI_LABEL)
+
+clean_output = []
+
+for edct, ddct in zip(entailment, disentailment):
+    nd = {}
+    nd["sequence"] = edct["sequence"]
+
+    if edct["sequence"] != ddct["sequence"]:
+        raise ValueError("entailment and disentailment sequences do not match")
+    
+    for idx, label in enumerate(edct["labels"]):
+        nd[label + "_ent"] = edct["scores"][idx]
+    for idx, label in enumerate(ddct["labels"]):
+        nd[label + "_dis"] = ddct["scores"][idx]
+
+    clean_output.append(nd)
+
+df = pd.DataFrame(clean_output)
+
+
+##### FIX THIS TO DEAL WITH HAVING BOTH SETS OF COLUMNS
+# mask to identify the columns with probabilities
+prob_cols = df.columns.difference(['sequence'])
+# creating a copy to normalize
+df2 = df.copy(deep=False) 
+# Normalizing each row
+df2[prob_cols] = normalize(df[prob_cols].values, axis=1)
+# combining probabilities with the original data
+output =pd.concat([data, df2], axis=1)
+
+# writing to csv
+output.to_csv(OUTPUT_PATH)
+
+
